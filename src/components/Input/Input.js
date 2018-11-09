fe22d31fecf542331d262f220a82241c95ec21ff
StanislavkoP <stas8327634@gmail.com>
2018-11-09 12:25:24 +0200

    Added props for classes

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './Input.css';
 
 const Input = props => {
-	let classes = 'ui fluid icon input';
-	let propsClasses;
 
+	let propsClasses;
 	if (props.classes !== 'undefined') {
 		propsClasses = props.classes;
 	} 
 
-
-
 	return (
-		<div className="ui fluid icon input">
-			<input className={classes + ' ' + propsClasses } type="text" placeholder={props.placeholder} onChange={props.change}/>
-		</div>
+		<input className={ propsClasses } type="text" placeholder={props.placeholder} onChange={props.change}/>
 	)
 }
 
