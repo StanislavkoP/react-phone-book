b938f6d2144534de04590621c6e7220d4f587c98
StanislavkoP <stas8327634@gmail.com>
2018-11-09 12:28:37 +0200

    Redesigned code

--- src/components/PhoneList/PhoneList.js
+++ src/components/PhoneList/PhoneList.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PhoneItem from './PhoneItem/PhoneItem';
 
 const PhoneList = props => (
-	<div className="ui centered cards">
+	<div className="ui cards">
 		{
-			props.phoneList.map( phone => {
-				return <PhoneItem
-					key={phone.id} 
-					firstName={phone.firstName}
-					lastName={phone.lastName}
-					photo={phone.photo}
-					company={phone.company}
-					contacts={phone.contacts}
+			props.phoneList.map( phone => (
+					<PhoneItem
+						key={phone.id} 
+						firstName={phone.firstName}
+						lastName={phone.lastName}
+						photo={phone.photo}
+						company={phone.company}
+						contacts={phone.contacts}
 					/>
-			})
+				)
+			)
 		}
 	</div>
 );
