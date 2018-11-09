9aa9ed9f022e2f88f01df7a5e6a0a4af0e17f83a
StanislavkoP <stas8327634@gmail.com>
2018-11-09 12:28:09 +0200

    Added form and function for adding contact

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,25 +1,104 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import { debounce } from '../shared/index';
+import { debounce, updateObject, checkValidity } from '../shared/index';
 
 import * as actions from '../state/actions/actions';
 
 import PhoneList from '../components/PhoneList/PhoneList';
+import AddContactForm from '../components/AddContactForm/AddContactForm';
 import Spinner from '../components/Spinner/Spinner';
 import Input from '../components/Input/Input';
 
 class Dashboard extends Component {
 
+	state = {
+		inputs: {
+			firstName: {
+				inputIdentifier: 'firstName',
+				value: '',
+				validation: {
+					isName: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+			lastName: {
+				inputIdentifier: 'lastName',
+				value: '',
+				validation: {
+					isLastName: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+			photo: {
+				inputIdentifier: 'photo',
+				value: '',
+				validation: {
+					isPhotoLink: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+			company: {
+				inputIdentifier: 'company',
+				value: '',
+				validation: {
+					isCompany: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+			email: {
+				inputIdentifier: 'email',
+				value: '',
+				validation: {
+					isEmail: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+			phoneNumber: {
+				inputIdentifier: 'phoneNumber',
+				value: '',
+				validation: {
+					isPhoneNumber: true
+				},
+				valid: {
+					isValid: false,
+					errorMessage: null
+				},
+				touched: false
+			},
+		},
+		formIsValid: false
+	}
+
 	componentDidMount () {
 		this.props.loadPhoneList()
 	}
 
-	searchPost = debounce(value => {
+	searchPost = debounce((value) => {
 		const inputValue = value.trim();
 
 		if (inputValue) {
-			this.props.searchPhone(inputValue)
+			this.props.searchPhone(inputValue, this.props.phonesListInit)
 
 		} else {
 			this.props.viewAllPhones()
@@ -27,12 +106,79 @@ class Dashboard extends Component {
 
 	}, 300)
 
+	changeInputHandler = (event, inputIdentifier) => {
+
+		const updatedFormElement = updateObject(this.state.inputs[inputIdentifier], {
+			value: event.target.value,
+			valid: checkValidity(event.target.value, this.state.inputs[inputIdentifier].validation),
+			touched: true
+		});
+		
+			const updatedOrderForm = updateObject(this.state.inputs, {
+			[inputIdentifier]: updatedFormElement
+		}); 
+		
+		let formIsValid = true;
+		for (let inputIdentifier in updatedOrderForm) {
+			formIsValid = updatedOrderForm[inputIdentifier].valid.isValid && formIsValid;
+		}
+		
+		this.setState({inputs: updatedOrderForm, formIsValid: formIsValid});
+	}
+
+	addContact = ( event ) => {
+		event.preventDefault();
+		let contactData = {};
+
+		for (let formElementIdentifier in this.state.inputs) {
+			contactData[formElementIdentifier] = this.state.inputs[formElementIdentifier].value
+		}
+		
+		contactData = {
+			id: new Date().getTime(),
+			firstName: contactData.firstName,
+			lastName: contactData.lastName,
+			company: contactData.company,
+			contacts: {
+				email: contactData.email,
+				phoneNumber: contactData.phoneNumber,
+			},
+			photo: contactData.photo
+
+		}
+
+		this.props.onAddContact(contactData)
+	}
+
 
 	render () {
-
+		console.log(this.props.newContactLoading);
 		let phoneList = <Spinner />
 		if (this.props.loading === false) {
-			phoneList = <PhoneList phoneList={ this.props.filteredPhoneList }/>
+			phoneList = (
+				<React.Fragment>
+					<AddContactForm
+						change={ (e, inputIdentifier) => this.changeInputHandler(e, inputIdentifier) }
+						addContact={ this.addContact }
+
+						dataOfName={ this.state.inputs.firstName }
+						dataOfLastName={ this.state.inputs.lastName }
+						dataOfCompany={ this.state.inputs.company }
+						dataOfPhoto={ this.state.inputs.photo }
+						dataOfPhoneNumber={ this.state.inputs.phoneNumber }
+						dataOfEmail={ this.state.inputs.email }
+						
+						disablingForm={ !this.state.formIsValid }
+						loading={this.props.loadingNewContact}
+					/>
+
+					<div className="ui input fluid focus">
+						<Input classes="searchPhone big" placeholder='Search' change={(e) => this.searchPost(e.currentTarget.value)} />
+					</div>
+
+					<PhoneList phoneList={ this.props.filteredPhoneList }/>
+				</React.Fragment>
+			)
 		}
 
 		let error = null;
@@ -42,7 +188,6 @@ class Dashboard extends Component {
 
 		return (
 			<React.Fragment>
-				<Input classes="searchPhone" placeholder='Search' change={(e) => this.searchPost(e.currentTarget.value)} />
 				{ phoneList }
 				{ error }
 			</React.Fragment>
@@ -66,9 +211,12 @@ Dashboard.propTypes = {
 
 
 const mapStateToProps = state => {
+	console.log(state)
 	return {
+		phonesListInit: state.phonesInit,
 		filteredPhoneList: state.filteredPhones,
 		loading: state.loading,
+		loadingNewContact: state.loadingNewContact,
 		error: state.error
 	}
 }
@@ -76,7 +224,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
 	return {
 		loadPhoneList: () => dispatch( actions.loadPhoneList() ),
-		searchPhone: (value) => dispatch( actions.searchPhone(value) ),
+		onAddContact: (contactData) => dispatch( actions.onAddContact(contactData) ),
+		searchPhone: (value, phoneListInit) => dispatch( actions.searchPhone(value, phoneListInit) ),
 		viewAllPhones: () => dispatch( actions.viewAllPhones() )
 	}
 }
