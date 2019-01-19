dc6c4eca4da450459c59f4e28a829d910d7c9c9f
StanislavkoP <stas8327634@gmail.com>
2019-01-19 11:20:24 +0200

    Added style for container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './containers/Dashboard';
 class App extends Component {
   render() {
     return (
-      <div className="ui container">
+      <div className="ui container" style={{marginTop: '16px'}}>
         <Dashboard/>
       </div>
     );
