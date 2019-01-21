140ad75f8cdb354956ea2f80d1d3047b48374bcb
StanislavkoP <stas8327634@gmail.com>
2019-01-21 13:03:47 +0200

    Added propTypes and refactored code

--- src/components/AddContactForm/AddContactField/AddContactField.js
+++ src/components/AddContactForm/AddContactField/AddContactField.js
@@ -1,21 +1,54 @@
 import React from 'react';
 
-const AddContactField = props => {
+import PropTypes from 'prop-types';
+
+AddContactField.propTypes = {
+	inputValue: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	label: PropTypes.string,
+	isRequired: PropTypes.bool,
+	isValid: PropTypes.bool,
+	touched: PropTypes.bool,
+	errorMessage: PropTypes.string,
+
+	change: PropTypes.func.isRequired,
+}
+
+function AddContactField (props) {
+	const {
+		inputValue,
+		placeholder,
+		label,
+		isRequired,
+		isValid,
+		touched,
+		errorMessage,
+
+		change,
+
+	} = props
+
 	let errorClass = ''
-	if ( props.isValid === false && props.touched ) {
+	if ( isValid === false && touched ) {
 		errorClass = 'error';
 	}
 
-	return(
+	return (
 		<div className={`field ${ errorClass }`}>
-			<label>{ props.label } {props.isRequired ? <i className="asterisk red icon"></i> : null}</label>
-			<input type="text"  
-				placeholder={ props.placeholder } 
-				value={ props.inputValue }
-				onChange={ props.change }
+			<label>
+				{ label } { isRequired ? <i className="asterisk red icon"></i> : null }
+			</label>
+			
+			<input 
+				type="text"  
+				placeholder={ placeholder } 
+				value={ inputValue }
+				onChange={ change }
 			/>
-			<p>{ props.errorMessage }</p>
+
+			<p>{ errorMessage }</p>
 		</div>
 	)
-}
+};
+
 export default AddContactField;
