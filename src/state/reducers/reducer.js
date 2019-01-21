bc54d6dc168a918de88b8d21f51590c97f2c936b
StanislavkoP <stas8327634@gmail.com>
2019-01-21 11:39:21 +0200

    Added to store edited contact`s id and refactored code

--- src/state/reducers/reducer.js
+++ src/state/reducers/reducer.js
@@ -2,119 +2,125 @@ import * as actionTypes from '../actions/actionTypes';
 
 
 const initialState = {
-	phonesInit: [],
+	phoneList: [],
+	editedContactId: null,
 	searchedText: '',
-	loading: false,
-	loadingNewContact: false,
+	loadingPhoneList: false,
+	loadingForm: false,
 	error: null
 }
 
-const loadPhoneListStart = (state, action) => {
+const loadPhoneListInit = (state, action) => {
 	return {
 		...state,
-		loading: true
+		loadingPhoneList: true
 	}
 }
 
 const loadPhoneListSucces = (state, action) => {
 	return {
 		...state,
-		phonesInit: [
+		phoneList: [
 			...action.phoneList
 		],
-		filteredPhones: [
-			...action.phoneList
-		],
-		loading: false
+		loadingPhoneList: false
 	}
 }
 
 const loadPhoneListFailed = (state, action) => {
 	return {
 		...state,
-		loading: false,
+		loadingPhoneList: false,
 		error: action.error
 	}
 }
 
-const onAddContactStart = (state, action) => {
+const onAddContactInit = (state, action) => {
 	return {
 		...state,
-		loadingNewContact: true
+		loadingForm: true
 	}
 }
 
 const onAddContactSucces = (state, action) => {
 	return {
 		...state,
-		phonesInit: [
-			...state.phonesInit,
+		phoneList: [
+			...state.phoneList,
 			action.newContact
 		],
-		filteredPhones: [
-			...state.filteredPhones,
-			action.newContact
-		],
-		loadingNewContact: false
+		loadingForm: false
 	}
 }
 
 const onAddContactFailed = (state, action) => {
 	return {
 		...state,
-		loadingNewContact: false,
+		loadingForm: false,
 		error: action.error
 	}
 }
 
 const onUpdateContactSuccess = (state, action) => ({
 	...state,
-	phonesInit: action.updatedPhoneList,
+	phoneList: action.updatedPhoneList,
+	loadingForm: false,
 });
 
 const onUpdateContactFailed = (state, action) => ({
 	...state,
+	loadingForm: false,
 	error: action.error,
+});
+
+const onEditContact = (state, action) => ({
+	...state,
+	editedContactId: action.contactId
+});
+
+const onDeleteContactInit = (state, action) => ({
+	...state,
+	loadingPhoneList: true
 });
 
 const onDeleteContactSuccess = (state, action) => ({
 	...state,
-	phonesInit: [
+	phoneList: [
 		...action.newPhoneList
 	],
-	filteredPhones: [
-		...action.newPhoneList
-	],
-	loading: false
+	loadingPhoneList: false
 });
 
 const onDeleteContactFailed = (state, action) => ({
 	...state,
+	loadingPhoneList: false,
 	error: action.error,
-	loading: false
 });
 
 const searchPhone = (state, action) => {
 	return {
 		...state,
 		searchedText: action.searchedText,
-		loading: false
 	}
 }
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case actionTypes.LOAD_PHONE_LIST_START : return loadPhoneListStart(state, action);
+		case actionTypes.LOAD_PHONE_LIST_INIT : return loadPhoneListInit(state, action);
 		case actionTypes.LOAD_PHONE_LIST_SUCCESS : return loadPhoneListSucces(state, action);
 		case actionTypes.LOAD_PHONE_LIST_FAILED : return loadPhoneListFailed(state, action);
 		
-		case actionTypes.ADD_CONTACT_START : return onAddContactStart(state, action);
+		case actionTypes.ADD_CONTACT_INIT : return onAddContactInit(state, action);
 		case actionTypes.ADD_CONTACT_SUCCESS : return onAddContactSucces(state, action);
 		case actionTypes.ADD_CONTACT_FAILED : return onAddContactFailed(state, action);
 		
+		case actionTypes.EDIT_CONTACT : return onEditContact(state, action);
+		
+		case actionTypes.UPDATE_CONTACT_INIT : return onUpdateContactSuccess(state, action);
 		case actionTypes.UPDATE_CONTACT_SUCCESS : return onUpdateContactSuccess(state, action);
 		case actionTypes.UPDATE_CONTACT_FAILED : return onUpdateContactFailed(state, action);
 		
+		case actionTypes.DELETE_CONTACT_INIT : return onDeleteContactInit(state, action);
 		case actionTypes.DELETE_CONTACT_SUCCESS : return onDeleteContactSuccess(state, action);
 		case actionTypes.DELETE_CONTACT_FAILED : return onDeleteContactFailed(state, action);
 		
